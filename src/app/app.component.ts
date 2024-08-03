8f6afc50d479ff4686441466a1967aca049b19e9
ramyroo <ramyroo420@gmail.com>
2024-08-03 19:34:52 +0300

    initial commit for project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { FooterComponent } from './footer/footer.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NavbarComponent,AboutComponent,FooterComponent,PortfolioComponent,ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { NotFoundComponent } from './not-found/not-found.component';
+import { AboutComponent } from './about/about.component';
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { ContactComponent } from './contact/contact.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"",component:AboutComponent,title:"home"},
+    { path: "about"||"", component: AboutComponent,title:"about" },
+    { path: "contact", component: ContactComponent,title:"contact" },
+    { path: "portfolio", component: PortfolioComponent,title:"portfolio" },
+    {path:"**",component:NotFoundComponent,title:"not found"}
+];
